caca1c49a323dd853690e3e178bd390863daa670
DK <4artit@gmail.com>
2020-08-13 02:24:21 +0900

    implement texture class constructor

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,5 +1,35 @@
 #include "Texture.h"
+#include "vender/stb_image/stb_image.h"
 
-class Texture {
+Texture::Texture(const std::string& path)
+	: renderer_id_(0), file_path_(path), local_buffer_(nullptr), width_(0), height_(0), bpp_(0)
+{
+	stbi_set_flip_vertically_on_load(1);
+	local_buffer_ = stbi_load(path.c_str(), &width_, &height_, &bpp_, 4);
+	GLCall(glGenTextures(1, &renderer_id_));
+	GLCall(glBindTexture(GL_TEXTURE_2D,renderer_id_));
 
-};
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP));
+
+	GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width_, height_, 0, GL_RGBA, GL_UNSIGNED_BYTE, local_buffer_));
+	GLCall(glBindTexture(GL_TEXTURE_2D, 0));
+	
+	if (local_buffer_) {
+		stbi_image_free(local_buffer_);
+	}
+}
+
+Texture::~Texture()
+{
+}
+
+void Texture::Bind(unsigned int slot) const
+{
+}
+
+void Texture::Unbind() const
+{
+}
--- src/Texture.h
+++ src/Texture.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Renderer.h"
+#include <string>
+
+class Texture {
+public:
+	Texture(const std::string& path);
+	~Texture();
+
+	void Bind(unsigned int slot = 0) const;
+	void Unbind() const;
+
+	inline int GetWidth() const { return width_; }
+	inline int GetHeight() const { return height_; }
+private:
+	unsigned int renderer_id_;
+	std::string file_path_;
+	unsigned char* local_buffer_;
+	int width_, height_, bpp_; // bbp : byte per pixel
+};
