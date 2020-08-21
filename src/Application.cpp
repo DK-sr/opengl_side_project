5413e96becee0619be0bacb2ca967de8541afe9c
DK <4artit@gmail.com>
2020-08-21 23:29:56 +0900

    WIP:: adding new mvp for making one more object

--- src/Application.cpp
+++ src/Application.cpp
@@ -51,10 +51,10 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[] = {
-         100.0f, 100.0f, 0.0f, 0.0f,// 0
-         100.0f, 200.0f, 1.0f, 0.0f,// 1
-         200.0f, 200.0f, 1.0f, 1.0f,// 2
-         200.0f, 100.0f, 0.0f, 1.0f // 3
+         -50.0f, -50.0f, 0.0f, 0.0f,// 0
+         -50.0f,  50.0f, 1.0f, 0.0f,// 1
+          50.0f,  50.0f, 1.0f, 1.0f,// 2
+          50.0f, -50.0f, 0.0f, 1.0f // 3
     };
 
     unsigned int indices[] = {
@@ -76,7 +76,7 @@ int main(void)
     std::unique_ptr<IndexBuffer> ib = std::make_unique<IndexBuffer>(indices, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
     std::unique_ptr<Shader> shader = std::make_unique<Shader>("res/shaders/Basic.glsl");
     shader->Bind();
@@ -106,7 +106,8 @@ int main(void)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     float r = 0.0f;
-    glm::vec3 translation(200, 200, 0);
+    glm::vec3 translationA(200, 200, 0);
+    glm::vec3 translationB(400, 400, 0);
     float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -123,7 +124,9 @@ int main(void)
         glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
         glm::mat4 mvp = proj * view * model;
         shader->SetUniformMat4f("u_MVP", mvp);
+        renderer.Draw(*va, *ib, *shader);
 
+        shader->SetUniformMat4f("u_MVP", mvp);
         renderer.Draw(*va, *ib, *shader);
 
         if (r > 1.0f) {
