778d050d9013302fad8580692a9c30c869d6d29c
DK <4artit@gmail.com>
2020-08-24 01:48:39 +0900

    refactor the code and added color test

--- src/Application.cpp
+++ src/Application.cpp
@@ -19,11 +19,12 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
     GLFWwindow* window;
 
-    /* Initialize the library */
     if (!glfwInit())
         return -1;
 
@@ -31,7 +32,6 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(960, 540, "GL STUDY", NULL, NULL);
     if (!window)
     {
@@ -39,7 +39,6 @@ int main(void)
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
     glfwSwapInterval(1);
@@ -50,47 +49,8 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[] = {
-         -50.0f, -50.0f, 0.0f, 0.0f,// 0
-         -50.0f,  50.0f, 1.0f, 0.0f,// 1
-          50.0f,  50.0f, 1.0f, 1.0f,// 2
-          50.0f, -50.0f, 0.0f, 1.0f // 3
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2,
-        2, 3, 0
-    };
-
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-    std::unique_ptr<VertexArray> va = std::make_unique<VertexArray>();
-    std::unique_ptr<VertexBuffer> vb = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
-
-    std::unique_ptr<VertexBufferLayout> layout = std::make_unique<VertexBufferLayout>();
-    layout->Push<float>(2);
-    layout->Push<float>(2);
-    va->AddBuffer(*vb, *layout);
-
-    std::unique_ptr<IndexBuffer> ib = std::make_unique<IndexBuffer>(indices, 6);
-
-    glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-    std::unique_ptr<Shader> shader = std::make_unique<Shader>("res/shaders/Basic.glsl");
-    shader->Bind();
-    shader->SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-    std::unique_ptr<Texture> texture = std::make_unique<Texture>("res/texture/sample.png");
-    texture->Bind();
-    shader->SetUniform1i("u_Texture", 0);
-
-    //Unbind shader, index buffer and array buffer
-    va->Unbind();
-    vb->Unbind();
-    ib->Unbind();
-    shader->Unbind();
 
     Renderer renderer;
 
@@ -101,78 +61,29 @@ int main(void)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
+    test::TestClearColor test;
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    float r = 0.0f;
-    glm::vec3 translationA(200, 200, 0);
-    glm::vec3 translationB(400, 400, 0);
-    float increment = 0.05f;
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
         renderer.Clear();
+
+        test.OnUpdate(0.0f);
+        test.OnRender();
 
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-            glm::mat4 mvp = proj * view * model;
-            shader->Bind();
-            shader->SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(*va, *ib, *shader);
-        }
-
-        {
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-            glm::mat4 mvp = proj * view * model;
-            shader->Bind();
-            shader->SetUniformMat4f("u_MVP", mvp);
-            renderer.Draw(*va, *ib, *shader);
-        }
-
-        if (r > 1.0f) {
-            increment = -0.05f;
-        }
-        else if (r < 0.0f) {
-            increment = 0.05f;
-        }
-        r += increment;
-
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Example", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
-
-            ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.f);
-            ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.f);
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-
-            ImGui::End();
-        }
-
+        test.OnImGuiRender();
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-        /* Poll for and process events */
         glfwPollEvents();
     }
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
-    vb.reset();
-    ib.reset();
-    layout.reset();
-    va.reset();
-    texture.reset();
-    shader.reset();
     glfwTerminate();
     return 0;
 }
